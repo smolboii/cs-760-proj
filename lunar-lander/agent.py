a394028a9779faf7eb5ce9343c066bbc620ce0fa
loldabigboi <loldabigboi@gmail.com>
2021-05-26 17:35:05 +1200

    updated lunar lander architecture to be mlp with no head

--- lunar-lander/agent.py
+++ lunar-lander/agent.py
@@ -1,6 +1,8 @@
 import numpy as np
+import torch.nn.functional as F
 import torch.nn as nn
 import random
+from collections import deque
 import kerasncp as kncp
 from kerasncp.torch import LTCCell
 import torch
@@ -9,46 +11,19 @@ import torch.utils.data as data
 class NCPNetwork(nn.Module):
     def __init__(
         self,
-        ncp_cell
+        in_features,
+        n_actions,
+        config_obj,
+        device
     ):
         super(NCPNetwork, self).__init__()
-        self.ncp_cell = ncp_cell
 
-    def forward(self, x):
-
-        device = x.device
-        batch_size = x.size(0)
-        hidden_state = torch.zeros(
-            (batch_size, self.ncp_cell.state_size), device=device
-        )
-
-        output, _ = self.ncp_cell(x, hidden_state)
-        return output
-
-class DQNetwork(nn.Module):
-    def __init__(self, in_features, out_features):
-        super(DQNetwork, self).__init__()
-
-        self.layers = nn.Sequential(
+        self.backbone_fcn = nn.Sequential(
             nn.Linear(in_features, 256),
             nn.ReLU(),
             nn.Linear(256, 256),
-            nn.ReLU(),
-            nn.Linear(256, 20),
-            nn.ReLU(),
-            nn.Linear(20, 10),
-            nn.ReLU(),
-            nn.Linear(10, out_features),
+            nn.ReLU()
         )
-
-    def forward(self, x):
-        return self.layers(x)
-
-class Agent:
-    def __init__(self, in_features, n_actions, device, config_obj):
-
-        self.in_features = in_features
-        self.n_actions = n_actions
 
         wiring = kncp.wirings.NCP(
             inter_neurons=config_obj['inter_neurons'],  # Number of inter neurons
@@ -60,14 +35,53 @@ class Agent:
             # command neuron layer
             motor_fanin=config_obj['motor_fanin'],  # How many incoming synapses has each motor neuron
         )
-        ncp_cell = LTCCell(wiring, in_features)
+        ncp_cell = LTCCell(wiring, 256)
         ncp_cell.to(device)
 
-        self.model = NCPNetwork(ncp_cell)
-        #self.model = DQNetwork(in_features, n_actions)
+        self.ncp_cell = ncp_cell
+
+    def forward(self, x):
+
+        x = self.backbone_fcn(x)
+        device = x.device
+        batch_size = x.size(0)
+        hidden_state = torch.zeros(
+            (batch_size, self.ncp_cell.state_size), device=device
+        )
+
+        output, _ = self.ncp_cell(x, hidden_state)
+        return output
+
+class DQNetwork(nn.Module):
+    def __init__(self, in_features, n_actions):
+        super(DQNetwork, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(in_features, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 20),
+            nn.ReLU(),
+            nn.Linear(20, 10),
+            nn.ReLU(),
+            nn.Linear(10, n_actions)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+class Agent:
+    def __init__(self, in_features, n_actions, device, config_obj):
+
+        self.n_actions = n_actions
+
+        #self.model = NCPNetwork(in_features, n_actions, config_obj, device)
+        self.model = DQNetwork(in_features, n_actions)
         self.model.to(device)
-        self.target_model = NCPNetwork(ncp_cell)
-        #self.target_model = DQNetwork(in_features, n_actions)
+
+        #self.target_model = NCPNetwork(in_features, n_actions, config_obj, device)
+        self.target_model = DQNetwork(in_features, n_actions)
         self.target_model.to(device)
         self.target_model.load_state_dict(self.model.state_dict())
 
@@ -82,35 +96,53 @@ class Agent:
         self.epsilon_decay = config_obj['epsilon_decay']
         self.min_epsilon = 0.01
 
+        self.min_mem = config_obj['min_mem']
+        self.replay_mem = deque(maxlen=config_obj['max_mem'])
+        self.batch_size = config_obj['batch_size']
+
         self.device = device
 
-    def get_action(self, state, eval=False):
-        if eval or random.random() > self.epsilon:
+    def get_action(self, state, train=True):
+        if not train or random.random() > self.epsilon:
             with torch.no_grad():
-                return torch.argmax(self.model(torch.tensor([state]).float().to(self.device))).item()
+                state = torch.tensor([state]).to(self.device)
+                actions = self.model(state.float())
+                return torch.argmax(actions).item()        
         else:
             return random.randrange(0, self.n_actions)
 
-    def train(self, transitions):
+    
+    def update_mem(self, transition):
+        self.replay_mem.append(transition)
 
-        state_batch = torch.tensor([t.state for t in transitions]).float().to(self.device)
-        new_state_batch = torch.tensor([t.new_state for t in transitions]).float().to(self.device)
-        reward_batch = torch.tensor([t.reward for t in transitions]).to(self.device)
-        done_batch = torch.tensor([t.done for t in transitions]).to(self.device)
-        action_batch = [t.action for t in transitions]
+    def train(self):
 
-        q_vals = self.model(state_batch)[np.arange(len(transitions)), action_batch]
+        if len(self.replay_mem) < self.min_mem:
+            return
+
+        self.optimizer.zero_grad()
+
+        batch_size = min(self.batch_size, len(self.replay_mem))
+        batch_index = np.random.choice(len(self.replay_mem), batch_size, replace=False)
+        batch = [self.replay_mem[i] for i in batch_index]
+
+        state_batch = torch.tensor([t.state for t in batch]).float().to(self.device)
+        new_state_batch = torch.tensor([t.new_state for t in batch]).float().to(self.device)
+        reward_batch = torch.tensor([t.reward for t in batch]).to(self.device)
+        done_batch = torch.tensor([t.done for t in batch]).to(self.device)
+        action_batch = [t.action for t in batch]
+
+        q_vals = self.model(state_batch)[np.arange(batch_size), action_batch]
         q_next = self.target_model(new_state_batch)
         q_next[done_batch] = 0.0
         
         q_targets = reward_batch + self.discount_factor * torch.max(q_next, dim=1)[0]
 
-        self.optimizer.zero_grad()
-        loss = self.loss_fn(q_vals, q_targets)
+        loss = self.loss_fn(q_vals.float(), q_targets.float())
         loss.backward()
         self.optimizer.step()
 
-        self.model.ncp_cell.apply_weight_constraints()
+        #self.model.ncp_cell.apply_weight_constraints()
 
         self.epsilon = max(self.epsilon * self.epsilon_decay, self.min_epsilon)
 
--- lunar-lander/main.py
+++ lunar-lander/main.py
@@ -3,7 +3,6 @@ import numpy as np
 import torch
 from transition import Transition
 from agent import Agent
-from collections import deque
 import argparse
 import os
 import time
@@ -20,12 +19,14 @@ def complete_config(config_obj):
         'recurrent_command_synapses': 0,
         'motor_fanin': 6,
         'discount_factor': 0.99,
-        'epsilon_decay': 0.99975,
+        'epsilon_decay': 0.9975,
         'min_epsilon': 0.01,
         "lr": 0.01,
-        "lr_decay": 0.1,
+        "lr_decay_rate": 0.1,
         "lr_decay_interval": 200,
-        'batch_size': 64
+        'batch_size': 64,
+        'min_mem': 1_000,
+        'max_mem': 1_000_000
     }
 
     # add defaults to config obj if values were not specified
@@ -79,13 +80,9 @@ if __name__ == '__main__':
 
     start_time = round(time.time())
 
-    transitions = deque(maxlen=1_000_000)
     num_episodes = args.num_episodes
     chkpt_every = args.checkpoint_interval
     sum_rewards = []
-
-    batch_size=config_obj['batch_size']
-    min_transitions = 1_000
 
     iterations = 0
     lr = config_obj['lr']
@@ -96,8 +93,8 @@ if __name__ == '__main__':
         ep_rewards = []
 
         while not done:
-
-            action = agent.get_action(state, eval=args.vis)
+            
+            action = agent.get_action(state, train=(not args.vis))
             new_state, reward, done, _ = env.step(action)
             
             if args.vis:            
@@ -105,12 +102,11 @@ if __name__ == '__main__':
 
             ep_rewards.append(reward)
 
-            transitions.append(Transition(state, action, new_state, reward, done))
-            state = new_state
+            if not args.vis:
+                agent.update_mem(Transition(state, action, new_state, reward, done))
+                agent.train()
 
-            if len(transitions) > min_transitions and not args.vis:
-                iterations += 1
-                agent.train(np.random.choice(transitions, batch_size, replace=False))
+            state = new_state
 
         sum_rewards.append(np.sum(ep_rewards))
 
@@ -130,7 +126,7 @@ if __name__ == '__main__':
 
                 ep_rewards = []
                 while not done:
-                    action = agent.get_action(state, eval=True)
+                    action = agent.get_action(state, train=False)
                     new_state, reward, done, _ = env.step(action)
                     ep_rewards.append(reward)
                     state = new_state
